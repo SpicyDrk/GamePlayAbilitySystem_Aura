37af50f7ecf484e4cee8da3738600262e7b150c9
Brian Mcgrath <Champuster@gmail.com>
2023-10-15 16:07:56 -0400

    Animations

---
+++ Source/Aura/Private/Player/AuraPlayerContoller.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerContoller.h"
+
+AAuraPlayerContoller::AAuraPlayerContoller()
+{
+	bReplicates = true;
+	
+}
+
+void AAuraPlayerContoller::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
